d2d1ba4818b6148082baaa38af191bf9442812cb
slanja <97552526+slanja@users.noreply.github.com>
2024-02-18 21:33:06 +0100

    swaped CZ and US layout

--- scripts/keyboard_layout.sh
+++ scripts/keyboard_layout.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 case $(setxkbmap -query | grep layout | awk '{ print $2 }') in
-           cz) setxkbmap us && notify-send "Language set to: US";;
-           us) setxkbmap cz && notify-send "Langueg set to: CZ";;
+            us) setxkbmap cz && notify-send "Langueg set to: CZ";;
+            cz) setxkbmap us && notify-send "Language set to: US";;
             *) setxkbmap us && notify-send "Language set to: English-US";;
 esac
