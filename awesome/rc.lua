4c79bea173ab9d67a114aa8d0ed308c5d570ae1d
slanja <97552526+slanja@users.noreply.github.com>
2024-09-15 22:55:39 +0200

    recreated bar

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -128,33 +128,22 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 local clock_widget = wibox.widget {
     {
         {
-            align = "center",
-            valign = "center",
-            widget = wibox.widget.textclock('<span color="#ffffff" font="Motiva Sans 22" font-weight="bold">%H\n %M </span>', 5)
+            {
+                align = "center",
+                valign = "center",
+                forced_height = 80,
+                widget = wibox.widget.textclock('<span color="#ffffff" font="Motiva Sans 20" font-weight="bold">%H\n %M </span>', 5)
+            },
+            layout = wibox.layout.fixed.vertical
         },
-        layout = wibox.layout.fixed.vertical
+        bg = "#2b2b2b",
+        widget = wibox.container.background,
+        shape = gears.shape.octogon,
     },
-    bg = "#2b2b2b",
-    widget = wibox.container.background,
+    margins = 10,
+    widget = wibox.container.margin
 }
 
--- Create a wibox for each screen and add it
-local taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
-                )
 
 local tasklist_buttons = gears.table.join(
                      awful.button({ }, 1, function (c)
@@ -197,26 +186,67 @@ awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
     set_wallpaper(s)
 
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+
+    -- Apply margin and scale
+    s.mylayoutbox = wibox.widget {
+        s.mylayoutbox,
+        left = 15,
+        right = 15,
+        top = 10,
+        bottom = 15,
+        widget = wibox.container.margin
+    }
+
+
+    -- Set up buttons for the layoutbox
+    s.mylayoutbox:buttons(gears.table.join(
+        awful.button({}, 1, function () awful.layout.inc(1) end),
+        awful.button({}, 3, function () awful.layout.inc(-1) end),
+        awful.button({}, 4, function () awful.layout.inc(1) end),
+        awful.button({}, 5, function () awful.layout.inc(-1) end)
+    ))
+
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons,
-        layout = wibox.layout.fixed.vertical
+
+
+    -- Create a centered taglist widget with custom font
+    s.mytaglist = wibox.widget {
+        awful.widget.taglist {
+            screen  = s,
+            filter  = awful.widget.taglist.filter.all,
+            buttons = taglist_buttons,
+            layout = wibox.layout.fixed.vertical,
+            widget_template = {
+                {
+                    {
+                        id     = 'text_role',
+                        widget = wibox.widget.textbox,
+                    },
+                    widget = wibox.container.margin, -- Can adjust if needed, remove if no margin
+                },
+                id     = 'background_role',
+                widget = wibox.container.background,
+            },
+            -- Apply font settings
+            style = {
+                font   = "Motiva Sans 22",  -- Set the desired font and size
+            }
+        },
+        widget = wibox.container.place,
+        halign = 'center',  -- Center horizontally
+        valign = 'top',     -- Align to the top
     }
+
 
     -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist {
@@ -230,22 +260,28 @@ awful.screen.connect_for_each_screen(function(s)
     s.mywibox = awful.wibar({ position = "left", width = 70, screen = s })
     s.mywibox.visible = s.mywibox.visible
 
+    local battery_widget = require("battery")
+    local icon_widget = require("icon_widget")
+   
+
     -- Add widgets to the wibox
     s.mywibox:setup {
         layout = wibox.layout.align.vertical,
         { -- Left widgets
             layout = wibox.layout.fixed.vertical,
-            mylauncher,
+            --mylauncher,
+            icon_widget,
             s.mytaglist,
-            s.mypromptbox,
+            --s.mypromptbox,
         },
         not s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.vertical,
             -- mykeyboardlayout,
-            wibox.widget.systray(),
+            --wibox.widget.systray(),
+            battery_widget,
             clock_widget,
-            -- s.mylayoutbox,
+            s.mylayoutbox,
         },
     }
 end)
